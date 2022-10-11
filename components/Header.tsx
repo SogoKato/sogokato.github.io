4e0f4ff01b681576003912823eb81d9aa3852785
Sogo Kato <sogo.kato@outlook.com>
2022-10-11 03:38:46 +0000

    📱 Optimize header for sp

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,18 +9,18 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ className }) => {
   return (
     <header className={className}>
-      <div className="max-w-4xl mx-auto flex justify-between items-center h-48">
-        <div></div>
+      <div className="max-w-4xl mt-4 sm:mt-0 mx-auto flex justify-center sm:justify-between h-36 sm:h-48 items-center relative w-11/12">
+        <div className="hidden sm:block"></div>
         <Link href="/">
-          <a>
+          <a className="w-72 sm:w-auto">
             <Image src="/images/logo.svg" alt="logo" width="480" height="160" />
           </a>
         </Link>
-        <button id="themeSelector" className="h-8 text-center w-8">
+        <button id="themeSelector" className="absolute sm:static h-8 right-0 text-center top-0 w-6 sm:w-8">
           <img alt="テーマ切り替え" />
         </button>
       </div>
-      <Script src="/js/theme.js" />
+      <Script src="/js/theme.js" strategy="beforeInteractive" />
     </header>
   );
 };
