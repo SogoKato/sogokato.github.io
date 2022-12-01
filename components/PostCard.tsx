caf1bac03aab5dad6fe977793e88fb6608ef5824
Sogo Kato <sogo.kato@outlook.com>
2022-12-01 16:16:01 +0000

    ✨ Add syntax highlight

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import CodeBlock from "./CodeBlock";
 import Tags from "./Tags";
 import type { PostData } from "../types/post";
 
@@ -17,7 +18,7 @@ const PostCard: React.FC<PostCardProps> = ({ className, post, isPostPage, isStat
   const tagsEnd = <Tags className="mt-3" tags={post.tags}></Tags>;
   const titleClassName = "font-bold mt-3 text-4xl";
   const title = isPostPage ? <h1 className={"mb-3 " + titleClassName}>{post.title}</h1> : <h2 className={titleClassName}>{post.title}</h2>;
-  const content = isPostPage ? <ReactMarkdown remarkPlugins={[remarkGfm]}>{post.content}</ReactMarkdown> : <p className="line-clamp-3 my-5 text-neutral-600 dark:text-neutral-300">{post.desc}</p>;
+  const content = isPostPage ? <ReactMarkdown remarkPlugins={[remarkGfm]} components={{ code: CodeBlock }}>{post.content}</ReactMarkdown> : <p className="line-clamp-3 my-5 text-neutral-600 dark:text-neutral-300">{post.desc}</p>;
   const LikeButtons = dynamic(() => import("./Like"), { ssr: false });
   const SocialButtons = dynamic(() => import("./Social"), { ssr: false });
   const shareButtons = !isStaticPostPage ? <SocialButtons className="flex" path={post.ref} /> : null;
